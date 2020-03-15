808aeaf9b311c82629a84c26dc9c4df7370917f4
HyunMo Ahn <gusahdks@gmail.com>
2020-03-15 15:48:26 +0900

    Set Storybook config

---
+++ src/components/ChartComponent/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ChartComponent() {
+  return <div>Hello World</div>;
+}
+
+export default ChartComponent;
---
+++ src/stories/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ChartComponent from '../components/ChartComponent'
+
+export default { title: 'Chart' };
+
+export const chart = () => <ChartComponent />;
